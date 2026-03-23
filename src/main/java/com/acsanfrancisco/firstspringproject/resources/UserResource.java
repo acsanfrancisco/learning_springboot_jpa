c8d8ce21a7a554a7d8ef7f9c9642be145f105e9b
João Francisco Santiago <joaosannetof@gmail.com>
2026-03-23 19:09:58 -0300

    Resource Layer e Entidade User

---
+++ src/main/java/com/acsanfrancisco/firstspringproject/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.acsanfrancisco.firstspringproject.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acsanfrancisco.firstspringproject.entities.User;
+
+@RestController // indica que essa classe responde requisições HTTP ( @Controller, @ResponseBody)
+@RequestMapping("/user") // define qual a rota da URL (/user)
+public class UserResource {
+
+	@GetMapping // define que esse método responde requisições HTTP do tipo GET
+	public ResponseEntity<User> findAll(){ 
+		  // monta a resposta HTTP completa, cuida dos dados retornados, o status HTTP e informações extras (header)
+		
+		User user = new User(1L, "Maria", "maria@gmail.com", "998934323", "12345");
+		return ResponseEntity.ok().body(user); // .ok() define o status HTTP, .body() define o corpo da resposta
+	}
+}

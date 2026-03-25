32d3e78a73ee791de3e79a10ad0823deab9263c7
João Francisco Santiago <joaosannetof@gmail.com>
2026-03-25 20:23:12 -0300

    Camada de Serviço e Registro de Componentes

--- src/main/java/com/acsanfrancisco/firstspringproject/resources/UserResource.java
+++ src/main/java/com/acsanfrancisco/firstspringproject/resources/UserResource.java
@@ -1,21 +1,35 @@
 package com.acsanfrancisco.firstspringproject.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acsanfrancisco.firstspringproject.entities.User;
+import com.acsanfrancisco.firstspringproject.services.UserService;
 
 @RestController // indica que essa classe responde requisições HTTP (@Controller, @ResponseBody)
 @RequestMapping("/user") // define qual a rota da URL (/user)
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping // define que esse método responde requisições HTTP do tipo GET
-	public ResponseEntity<User> findAll(){ 
+	public ResponseEntity<List<User>> findAll(){ 
 		  // monta a resposta HTTP completa, cuida dos dados retornados, o status HTTP e informações extras (header)
 		
-		User user = new User(1L, "Maria", "maria@gmail.com", "998934323", "12345");
-		return ResponseEntity.ok().body(user); // .ok() define o status HTTP, .body() define o corpo da resposta
+		List<User> userList = service.findAll();
+		return ResponseEntity.ok().body(userList); // .ok() define o status HTTP, .body() define o corpo da resposta
+	}
+	
+	@GetMapping(value= "/{id}") //responde requisições do tipo GET com um ID na URL
+	public ResponseEntity<User> findById(@PathVariable Long id){  // pega o valor da URL
+		User u = service.findById(id);
+		return ResponseEntity.ok().body(u);
 	}
 }
---
+++ src/main/java/com/acsanfrancisco/firstspringproject/services/UserService.java
@@ -0,0 +1,27 @@
+package com.acsanfrancisco.firstspringproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.acsanfrancisco.firstspringproject.entities.User;
+import com.acsanfrancisco.firstspringproject.repositories.UserRepository;
+
+@Service // indica que a classe pertence a camada de serviços
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> opt = userRepository.findById(id);
+		return opt.get();
+	}
+
+}

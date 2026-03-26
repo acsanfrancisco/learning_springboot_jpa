4c2aaed6cdc85bc11b88bf687dc6ce4f1b5c57b0
João Francisco Santiago <joaosannetof@gmail.com>
2026-03-26 18:55:12 -0300

    Tipo Enumerado OrderStatus

--- src/main/java/com/acsanfrancisco/firstspringproject/config/TestConfig.java
+++ src/main/java/com/acsanfrancisco/firstspringproject/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.acsanfrancisco.firstspringproject.entities.Order;
 import com.acsanfrancisco.firstspringproject.entities.User;
+import com.acsanfrancisco.firstspringproject.entities.enums.OrderStatus;
 import com.acsanfrancisco.firstspringproject.repositories.OrderRepository;
 import com.acsanfrancisco.firstspringproject.repositories.UserRepository;
 
@@ -36,9 +37,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.SHIPPED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED , u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT , u3);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}                            
